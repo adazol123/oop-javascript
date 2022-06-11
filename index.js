c7482d543c693db770ed7dd2d337b49a4a4cd866
Danyel <dpadayao40@gmail.com>
2022-06-12 06:08:32 +0800

    understanding oop in es5 and es6 version

---
+++ index.js
@@ -0,0 +1,57 @@
+//Object Literals
+
+// const people = {
+//     Name: "Danyel", 
+//     Age: 25,
+//     Gender: 'Male'
+// }
+
+// console.log(Object.keys(people))
+
+//ES5 Contructor method
+function People(Name, Age, Gender){
+    this.Name = Name;
+    this.Age = Age;
+    this.Gender = Gender;
+
+    // this.getInfo = function(){
+    //      return `${this.Name} is a ${this.Gender} and ${this.Age} years old.`
+    // }
+}
+//instanciate new object from contructor
+const people = new People('Danyel',26,'Male')
+
+//Create a prototype
+People.prototype.getInfo = function(){
+    return `${this.Name} is a ${this.Gender} and ${this.Age} years old.`
+}
+People.prototype.getAge = function(){
+    return `${this.Name} is ${this.Age} years old.`
+}
+
+
+//inheritance
+
+function Student(Name, Age, Gender, Year, Grade){
+
+    People.call(this, Name, Age, Gender);
+    this.Year = Year;
+    this.Grade = Grade;
+}
+
+Student.prototype = Object.create(People.prototype)
+
+Student.prototype.enroll = function (Year){
+    this.Year = Year;
+    this.enrolled = true;
+}
+
+const student = new Student('Josaphat', 21, 'Female', 2022, 'Grade 12')
+
+
+
+
+console.log(student.getAge())
+// console.log(student)
+// student.enroll(2023)
+// console.log(student)
---
+++ index2.js
@@ -0,0 +1,76 @@
+
+const People = {
+    getInfo : function(){
+        return `${this.Name} is a ${this.Gender} and ${this.Age} years old.`
+    }
+}
+
+//ES5 : Create Object v2
+const people = Object.create(People);
+
+people.Name = 'Juan';
+people.Age = 30;
+people.Gender = 'Male';
+
+
+//ES5 : Create Object v3
+const person = Object.create(People, {
+    Name: {value: 'Josaphat Lozada'},
+    Age: {value: 23},
+    Gender: {value: 'Female'}
+})
+
+console.log(people)
+console.log(person.getInfo())
+
+/*************************************************************
+ * 
+ * ES 6 version for Object oriented programming !!IMPORTANT
+ *
+ * ***********************************************************/
+
+
+// ES6 Constructor
+class Job {
+    constructor(Name, Age, Gender){
+        this.Name = Name;
+        this.Age = Age;
+        this.Gender = Gender;
+    }
+
+    getInfo(){
+        return `${this.Name} is a ${this.Gender} and ${this.Age} years old.`
+    }
+}
+
+class Employee extends Job{
+    constructor(Name, Age, Gender, YearHired, Role){
+        super(Name, Age, Gender)
+        this.YearHired = YearHired;
+        this.Role = Role;
+    }
+
+    info(YearHired){
+        this.YearHired = YearHired
+        this.hired = true;
+    }
+}
+
+
+//Instantiate
+const personWork = new Job('Daniel', 28, 'Male');
+const employee = new Employee("Adazol", 25, 'Female', 2019)
+
+
+console.log(personWork.getInfo())
+employee.info(2023)
+console.log(employee.getInfo())
+
+
+
+//Destructor
+const {Name, Age} = employee
+console.log(Age);
+
+
+
